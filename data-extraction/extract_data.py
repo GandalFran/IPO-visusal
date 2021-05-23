fa3fdb85b01742cb4e7ec6e7c24c65f9d9560da4
Francisco Pinto Santos <franpintosantos@usal.es>
2021-05-23 15:16:06 +0200

    .

--- data-extraction/extract_data.py
+++ data-extraction/extract_data.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
+output_folder = 'raw'
 dataset_name = 'vaccination_vs_income'
 
 with open('table.html','r') as f:
 	html_data = f.read()
 
 df = pd.read_html(html_data)[0]
-print(df)
-df.to_csv(f'{dataset_name}.csv',index=False)
+df.to_csv(f'{output_folder}/{dataset_name}.csv',index=False)
